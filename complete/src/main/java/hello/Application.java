4fb2a0f9f5a7404b4692bc96ff038a0ef43ae700
vishnu <vishnu.nair@datoms.io>
2022-03-07 01:46:46 +0530

    renaming file

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Docker World";
