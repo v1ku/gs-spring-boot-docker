52fb6abd626372473a586bab699ae12d420ee212
vishnu <vishnu.nair@datoms.io>
2022-03-07 01:07:28 +0530

    adding code

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,9 +1,12 @@
-package com.example.demo;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 class Calculator {
     
     public static int add(int x, int y){
