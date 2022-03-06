2c72354075a950fbbdc45eee684fb18706cbb58d
vishnu <vishnu.nair@datoms.io>
2022-03-07 00:50:12 +0530

    adding code

--- complete/src/test/java/hello/CalculationTest.java
+++ complete/src/test/java/hello/CalculationTest.java
@@ -1,22 +1,87 @@
-	
-package com.huongdanjava.springboot;
- 
-import org.junit.jupiter.api.Assertions;
+package com.demo.simplecalculator.backend.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
- 
-@SpringBootTest
-public class CalculationTest {
- 
-  @Autowired
-  private ApplicationContext context;
- 
-  @Test
-  public void testSum() {
-    Calculation calculation = context.getBean(Calculation.class);
- 
-    Assertions.assertEquals(9, calculation.sum(4, 5));
-  }
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SimpleCalculator.class})
+public class SimpleCalculatorTest {
+    
+    @Autowired
+    SimpleCalculator simpleCalculator;
+    
+    @Test
+    void executeAddEquation() {
+        int number1 = 10;
+        int number2 = 20;
+        
+        double result = this.simpleCalculator.add(number1, number2);
+        
+        assertEquals(result, 30);
+    }
+    
+    @Test
+    void executeSubtractEquation() {
+        int number1 = 10;
+        int number2 = 20;
+        
+        double result = this.simpleCalculator.subtract(number1, number2);
+        
+        assertEquals(result, -10);
+    }
+    
+    @Test
+    void executeMultiplyEquation() {
+        int number1 = 10;
+        int number2 = 20;
+        
+        double result = this.simpleCalculator.multiply(number1, number2);
+        
+        assertEquals(result, 200);
+    }
+    
+    @Test
+    void executeDivideEquation() {
+        int number1 = 10;
+        int number2 = 20;
+        
+        double result = this.simpleCalculator.divide(number1, number2);
+        
+        assertEquals(result, 0.5);
+    }
+    
+    @Test
+    void executeDivideEquationWithZeroDivisorForPositiveInfinite() {
+        int number1 = 10;
+        int number2 = 0;
+        
+        double result = this.simpleCalculator.divide(number1, number2);
+        
+        assertEquals(result, Double.POSITIVE_INFINITY);
+    }
+    
+    @Test
+    void executeDivideEquationWithZeroDivisorForNegativeInfinite() {
+        int number1 = -10;
+        int number2 = 0;
+        
+        double result = this.simpleCalculator.divide(number1, number2);
+        
+        assertEquals(result, Double.NEGATIVE_INFINITY);
+    }
+    
+    @Test
+    void executeDivideEquationWithZeroDivisorForNotANumber() {
+        int number1 = 0;
+        int number2 = 0;
+        
+        double result = this.simpleCalculator.divide(number1, number2);
+        
+        assertEquals(result, Double.NaN);
+    }
+    
 }
